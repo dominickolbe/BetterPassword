2ee220924c545efc749bca11f1beeaf62c3f0b50
Dominic Kolbe <kolbedominic@gmail.com>
2022-01-13 22:32:44 +0000

    init xcode project

---
+++ Better Password/Better_PasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_PasswordApp.swift
+//  Better Password
+//
+//  Created by Dominic Kolbe on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Better_PasswordApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
